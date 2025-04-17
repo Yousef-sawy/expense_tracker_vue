4ec60e01e26f6d943ff115435ca12461a25d7440
yousef sawy <yousefsawy2@gmail.com>
2025-04-17 15:37:23 +0200

    fix

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,18 +1,15 @@
 <template>
-    <h4>
-        Your Balance
+    <div>
+        <h4>Your Balance</h4>
         <h1 id="balance">${{ total }}</h1>
-    </h4>
+    </div>
 </template>
 
-<script setup> 
-import {defineProps} from 'vue';
-
+<script setup>
 const props = defineProps({
-    total:{
-        type:Number,
-        required:true,
+    total: {
+        type: Number,
+        required: true,
     },
 });
-
 </script>
